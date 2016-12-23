24d3391730a2a63fc3bf2e51ea9d7477302e31ed
Takashi Abe <bk89.ur327@gmail.com>
2016-12-23 15:17:42 +0900

    あんまり変わってない

--- com/title/TitlePanel.java
+++ com/title/TitlePanel.java
@@ -12,9 +12,10 @@ final public class TitlePanel extends JPanel implements ActionListener {
   JButton start;
   JButton deckEdit;
 
-  public TitlePanel(FrameController frameCont) { // FrameControllerでPanelを管理するために引数にこれをとる
+  public TitlePanel(FrameController frameCont) {
     this.frameCont = frameCont;
-    setLayout(new FlowLayout()); // とりあえず一番単純なFlowLayout()に設定。 後で変更するのも視野に入れておく
+    setLayout(new BorderLayout());
+    
     start = new JButton("ゲームスタート");
     deckEdit = new JButton("デッキ編集");
 
@@ -23,17 +24,81 @@ final public class TitlePanel extends JPanel implements ActionListener {
 
     this.add(start);
     this.add(deckEdit);
+  /*
+  setLayout(new GridBagLayout());
+
+  GridBagLayout layout = new GridBagLayout();
+  JPanel p = new JPanel();
+  p.setLayout(layout);
+
+  GridBagConstraints gbc = new GridBagConstraints();
+
+  JLabel label = new JLabel("じゃんけんゲーム", JLabel.CENTER);
+  gbc.gridx = 0;
+  gbc.gridy = 0;
+  gbc.gridheight = 2;
+  gbc.weightx = 1.0;
+  gbc.weighty = 1.0;
+  layout.setConstraints(label,gbc);
+
+  start = new JButton("ゲームスタート");
+  gbc.gridx = 1;
+  gbc.gridy = 0;
+  gbc.gridheight = 1;
+  gbc.weightx = 1.0;
+  gbc.weighty = 1.0;
+  layout.setConstraints(start, gbc);
+
+  deckEdit = new JButton("デッキ編集");
+  gbc.gridx = 1;
+  gbc.gridy = 1;
+  gbc.weightx = 1.0;
+  gbc.weighty = 1.0;
+  layout.setConstraints(deckEdit, gbc);
+
+  start.addActionListener(this);
+  deckEdit.addActionListener(this);
+
+  this.add(label);
+  this.add(start);
+  this.add(deckEdit);
+  */
+  /*
+  JLabel label = new JLabel("じゃんけんゲーム", JLabel.CENTER);
+  label.setSize(300,100);
+  label.setFont(new Font("Times New Roman",Font.BOLD | Font.ITALIC,12));
+
+  JPanel p = new JPanel();
+  p.setLayout(null);
+
+  JButton start = new JButton("Start");
+  start.setBounds(100,90,100,20);
+  start.setFont(new Font("Times New Roman",Font.BOLD | Font.ITALIC,8));
+
+  JButton deckEdit = new JButton("Edit");
+  deckEdit.setBounds(100,115,100,20);
+  deckEdit.setFont(new Font("Times New Roman",Font.BOLD | Font.ITALIC,8));
+
+  p.add(start);
+  p.add(deckEdit);
+
+  start.addActionListener(this);
+  deckEdit.addActionListener(this);
+
+  add(label);
+  add(p,BorderLayout.CENTER);
+
+  this.setVisible(true);
+
+  */
+
   }
 
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == start){
-      // メインゲーム画面への切り替え処理、大元のFrameControllerの中のメソッドを使う。
-      // 現在表示しているJPanelを破棄するため自分自身のインスタンス(this)を渡す。
       frameCont.showMainGame(this);
     }
     else if (e.getSource() == deckEdit) {
-      // デッキ編集画面への切り替え処理、大元のFrameControllerの中のメソッドを使う。
-      // 現在表示しているJPanelを破棄するため自分自身のインスタンス(this)を渡す。
       frameCont.showDeckEdit(this);
     }
   }
